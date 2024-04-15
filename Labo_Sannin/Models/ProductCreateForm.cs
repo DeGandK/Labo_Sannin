197398bfbb1b2f996dc39006aef5425f0e94a557
Kévin De Gand <degandk@gmail.com>
2024-04-15 15:41:10 +0200

    Save

---
+++ Labo_Sannin/Models/ProductCreateForm.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labo_Sannin_API.Models
+{
+    public class ProductCreateForm
+    {
+        [Required]
+        public string Nom {  get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Stock {  get; set; }
+        [Required]
+        public decimal PrixHTVA { get; set; }
+        [Required]
+        public string Image {  get; set; }
+        [Required]
+        public int CategorieID { get; set; }
+
+
+    }
+}
