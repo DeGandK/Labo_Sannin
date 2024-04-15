83dfda054cd48252cfc7ddb83eb6556ae761634e
j.monet <j.monet@GOS-VDI910>
2024-04-15 16:26:50 +0200

    CommandCreatForm et CommandControllers

---
+++ Labo_Sannin/Controllers/CommandController.cs
@@ -0,0 +1,40 @@
+﻿using Labo_BLL.Interfaces;
+using Labo_Sannin_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labo_Sannin_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly ICommandService _commandService;
+        public CommandController(ICommandService commandService)
+        {
+            _commandService = commandService;
+        }
+        /// <summary>
+        /// Fournit la liste des Commandes
+        /// </summary>
+        /// <returns>Un IEnumerable de Command</returns>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_commandService.GetAll());
+        }
+        [HttpGet("{UserID}")]
+        public IActionResult GetCommandsbyUserID([FromRoute]int UserID) 
+        {
+            return Ok(_commandService.GetCommandsByUserID(UserID));
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] CommandCreateForm form)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _commandService.Create(form.ToDomain());
+            return Ok();
+        }
+    }
+}
---
+++ Labo_Sannin/Models/CommandCreateForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labo_Sannin_API.Models
+{
+    public class CommandCreateForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }      
+        public string Type { get; set; }      
+    }
+}
