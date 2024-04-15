efd1ba181659f1f994a93a6ab590648de94898b4
Istvan prignot <istvanprignot@hotmail.com>
2024-04-15 15:41:50 +0200

    AuthController

---
+++ Labo_Sannin/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using Labo_BLL.Interfaces;
+using Labo_Domain.Models;
+using Labo_Sannin_API.Tools;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labo_Sannin_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly TokenGenerator _tokenGenerator;
+
+        public AuthController(IUserService userService, TokenGenerator tokenGenerator)
+        {
+            _userService = userService;
+            _tokenGenerator = tokenGenerator;
+
+        }
+        [HttpPost("login")]
+        public IActionResult Login(UserLoginForm loginInfo)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            try
+            {
+                User connectedUser = _userService.Login(loginInfo.Email, loginInfo.Password);
+                string token = _tokenGenerator.GenerateToken(connectedUser);
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserRegisterForm form)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            try
+            {
+                _userService.Register(form.Email, form.Password, form.Nickname);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- Labo_Sannin/Program.cs
+++ Labo_Sannin/Program.cs
@@ -1,6 +1,13 @@
 using Labo_DAL.Repositories;
 using DAL = Labo_DAL.Services;
+using BLL = Labo_BLL.Services;
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using Labo_BLL.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Labo_Sannin_API.Tools;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,18 +19,38 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUserRepo, DAL.UserService>();
+builder.Services.AddScoped<IUserService, BLL.UserService>();
 
 builder.Services.AddScoped<IProductRepo, DAL.ProductService>();
 
 builder.Services.AddScoped<ICommandRepo, DAL.CommandService>();
+builder.Services.AddScoped<TokenGenerator>();
 
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+        options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    builder.Configuration.GetSection("TokenInfo").GetSection("secretKey").Value)),
+                ValidateLifetime = true,
+                ValidAudience = "https://monclient.com",
+                ValidIssuer = "https://monapi.com",
+                ValidateAudience = false
+            };
+        }
+    );
+
 builder.Services.AddAuthorization(options =>
 {
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
+    options.AddPolicy("adminPolicy", policy => policy.RequireRole("admin"));
+    //options.AddPolicy("modoPolicy", policy => policy.RequireRole("admin", "moderator"));
+    //options.AddPolicy("adminPolicy", policy => policy.RequireClaim("UserId", "1");
+    options.AddPolicy("isConnectedPolicy", policy => policy.RequireAuthenticatedUser());
 });
 
 var app = builder.Build();
