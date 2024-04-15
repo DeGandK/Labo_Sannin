a71ab83787def4f486ea6b5e9acd41336e591dc2
Istvan prignot <istvanprignot@hotmail.com>
2024-04-15 15:10:55 +0200

    IUserServices

---
+++ Labo_BLL/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Labo_Domain.Models;
+
+namespace Labo_BLL.Interfaces
+{
+    public interface IUserService
+    {
+        List<User> GetAll();
+        User GetById(int id);
+        User Login(string email, string password);
+        void Register(string nom, string prenom, string email, string password, string telephone, string adresse);
+        void Update(User user);
+    }
+}
--- Labo_BLL/Services/UserService.cs
+++ Labo_BLL/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Labo_DAL.Repositories;
+﻿using Labo_BLL.Interfaces;
+using Labo_DAL.Repositories;
 using Labo_Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -8,17 +9,17 @@ using System.Threading.Tasks;
 
 namespace Labo_BLL.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
         private readonly IUserRepo _userRepo;
         public UserService(IUserRepo userRepo)
         {
             _userRepo = userRepo;
         }
-        public User Login(string email,string password) 
+        public User Login(string email, string password)
         {
             string VerifyPWD = _userRepo.GetHashPwd(email);
-            if (BCrypt.Net.BCrypt.Verify(password, VerifyPWD)) 
+            if (BCrypt.Net.BCrypt.Verify(password, VerifyPWD))
             {
                 try
                 {
@@ -36,17 +37,29 @@ namespace Labo_BLL.Services
                 throw new InvalidOperationException("Mots de passe incorrect");
             }
         }
-        public void Register(string nom, string prenom, string email,string password,string telephone,string adresse)
+        public void Register(string nom, string prenom, string email, string password, string telephone, string adresse)
         {
             string hashPWD = BCrypt.Net.BCrypt.HashPassword(password);
             try
             {
-                _userRepo.Register(nom, prenom, email,hashPWD,telephone,adresse);
+                _userRepo.Register(nom, prenom, email, hashPWD, telephone, adresse);
             }
             catch (Exception ex)
             {
                 throw ex;
             }
+        }
+        public void Update(User user)
+        {
+            _userRepo.Update(user);
+        }
+        public User GetById(int id)
+        {
+            return _userRepo.GetById(id);
+        }
+        public List<User> GetAll()
+        {
+            return _userRepo.GetAll();
         }
 
     }
