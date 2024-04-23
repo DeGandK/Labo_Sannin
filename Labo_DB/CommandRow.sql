e9de6a257d2ea82ae217b57aaaff07e5087cdab3
Nicolas Gemine <n.gemine@stag.technofuturtic.education>
2024-04-23 11:03:43 +0200

    Nico on delete cascade

--- Labo_DB/CommandRow.sql
+++ Labo_DB/CommandRow.sql
@@ -4,6 +4,6 @@
 	[CommandID] INT NOT NULL,
 	[ProductID] INT NOT NULL,
 	[Quantite] INT NOT NULL, 
-    CONSTRAINT [FK_CommandRow_Product] FOREIGN KEY ([ProductID]) REFERENCES [Product]([ProductID]), 
+    CONSTRAINT [FK_CommandRow_Product] FOREIGN KEY ([ProductID]) REFERENCES [Product]([ProductID]) ON DELETE CASCADE, 
     CONSTRAINT [FK_CommandRow_Command] FOREIGN KEY ([CommandID]) REFERENCES [Command]([CommandID])
 )
